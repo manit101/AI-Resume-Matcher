3e3e48b9b9dd600ef74ee844e8f9c5548ee0025b
manit101 <manit.p24@medhaviskillsuniversity.edu.in>
2026-05-03 21:23:18 +0530

    fix api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create Axios instance with base URL pointing to our Express server
 const API = axios.create({
-  baseURL: 'https://resume-matcher-backend-fntt.onrender.com/api',
+  baseURL: 'https://ai-resume-matcher-mihk.onrender.com/api',
 });
 
 // Interceptor to attach Firebase dummy token for auth middleware
